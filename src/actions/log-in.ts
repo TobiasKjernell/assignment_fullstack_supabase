b538f04591a853a5b1c65f6dbec72509608faf81
DenVitaKaninen <tobiaskjernell@gmail.com>
2025-10-08 03:22:26 +0200

    error update solution for login

--- src/actions/log-in.ts
+++ src/actions/log-in.ts
@@ -12,7 +12,7 @@ export const LogIn = async (userDataValues: z.infer<typeof logInSchema>) => {
     const supabase = await createClient();
     const { data: { user }, error } = await supabase.auth.signInWithPassword(parsedData);
 
-    if (error) return error;
+    if (error) return {error: error.message}
 
     redirect('/');
 }
